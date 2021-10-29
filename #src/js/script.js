c91d3878cf799424b24c1e2080b1d918a7974e1f
Dmitriy Kosenko <dimakosenko1998@gmail.com>
2021-10-29 11:26:48 +0300

    Прелоадер v1.1

--- #src/js/script.js
+++ #src/js/script.js
@@ -5,7 +5,7 @@ window.onload = function () {
   window.setTimeout(function () {
     document.body.classList.add('loaded');
     document.body.classList.remove('loaded_hiding');
-  }, 100);
+  }, 1000);
 }
 
 document.addEventListener("DOMContentLoaded", function () {
